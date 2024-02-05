06e84863d514d4f075c02345b025620436efdcb0
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-05 18:01:18 -0500

    creamos test para documentoservice2

--- src/main/java/Modulo/Resultados/Repositories/IDocumentacionRepository.java
+++ src/main/java/Modulo/Resultados/Repositories/IDocumentacionRepository.java
@@ -4,6 +4,7 @@ package Modulo.Resultados.Repositories;
 import Modulo.Resultados.Entity.Aspirante;
 import Modulo.Resultados.Entity.Documentacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,8 +12,9 @@ import java.util.UUID;
 
 @Repository
 public interface IDocumentacionRepository extends JpaRepository<Documentacion, UUID> {
-
+    @Query
     Optional<Documentacion> findByAspirante(Aspirante aspirante);
+    @Query
     Optional<Documentacion> findByAspiranteAndEstadoDocumentosTrue(Aspirante aspirante);
 
 
