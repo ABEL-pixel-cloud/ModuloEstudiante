bf23eb273611f95d97d4d391d20de24db0a9b9bd
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-06 07:58:43 -0500

    coreguir test

--- src/test/java/Modulo/Resultados/ResultadosApplicationTests.java
+++ src/test/java/Modulo/Resultados/ResultadosApplicationTests.java
@@ -2,8 +2,9 @@ package Modulo.Resultados;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = TestConfiguration.class)
 class ResultadosApplicationTests {
 
 	@Test
