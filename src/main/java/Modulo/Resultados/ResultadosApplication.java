671b221c097b6e967eaf76d6abbb98297fecca1e
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-06 15:58:32 -0500

    coreguir test

--- src/main/java/Modulo/Resultados/ResultadosApplication.java
+++ src/main/java/Modulo/Resultados/ResultadosApplication.java
@@ -2,10 +2,8 @@ package Modulo.Resultados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class ResultadosApplication {
 
 	public static void main(String[] args) {
