274d3c7d508b2a66d25c9cf89d7b2ee26f62589c
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-14 17:31:31 -0500

    creamos test para documentoservice2

--- src/main/java/Modulo/Resultados/Controllers/AspiranteController.java
+++ src/main/java/Modulo/Resultados/Controllers/AspiranteController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/aspirante/")
+@RequestMapping("/v1/aspirante")
 public class AspiranteController {
 
     private CrearAspiranteService service;
