dda20a7e63a5ba1f4896fac31373aaf09e6b6a3d
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-02-14 19:29:51 -0500

    creamos test para documentoservice2

--- src/main/java/Modulo/Resultados/Publisher/PublisherConfig.java
+++ src/main/java/Modulo/Resultados/Publisher/PublisherConfig.java
@@ -1,5 +1,6 @@
 package Modulo.Resultados.Publisher;
 
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
